ad342e5cd7d1139257dab1dc4d02cf0c6ff1bb83
Syatyakin Evgeny Dmitrievich <brom-nn@mail.ru>
2024-04-06 11:29:19 +0300

    >10
    >10

---
+++ Test/ui_dialog_window.h
@@ -0,0 +1,49 @@
+/********************************************************************************
+** Form generated from reading UI file 'dialog_window.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIALOG_WINDOW_H
+#define UI_DIALOG_WINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHeaderView>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Dialog_window
+{
+public:
+
+    void setupUi(QDialog *Dialog_window)
+    {
+        if (Dialog_window->objectName().isEmpty())
+            Dialog_window->setObjectName(QStringLiteral("Dialog_window"));
+        Dialog_window->resize(400, 300);
+
+        retranslateUi(Dialog_window);
+
+        QMetaObject::connectSlotsByName(Dialog_window);
+    } // setupUi
+
+    void retranslateUi(QDialog *Dialog_window)
+    {
+        Dialog_window->setWindowTitle(QApplication::translate("Dialog_window", "Dialog", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Dialog_window: public Ui_Dialog_window {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIALOG_WINDOW_H
---
+++ build-button_window-Desktop_Qt_5_7_0_MSVC2013_64bit-Debug/ui_secondwindow.h
@@ -0,0 +1,140 @@
+/********************************************************************************
+** Form generated from reading UI file 'secondwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_SECONDWINDOW_H
+#define UI_SECONDWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_SecondWindow
+{
+public:
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_14;
+    QSpacerItem *horizontalSpacer;
+    QVBoxLayout *verticalLayout_2;
+    QTextEdit *textEdit_4;
+    QTextEdit *textEdit_5;
+    QTextEdit *textEdit_15;
+
+    void setupUi(QDialog *SecondWindow)
+    {
+        if (SecondWindow->objectName().isEmpty())
+            SecondWindow->setObjectName(QStringLiteral("SecondWindow"));
+        SecondWindow->resize(767, 540);
+        QFont font;
+        font.setPointSize(14);
+        font.setBold(false);
+        font.setWeight(50);
+        SecondWindow->setFont(font);
+        SecondWindow->setLayoutDirection(Qt::LeftToRight);
+        SecondWindow->setInputMethodHints(Qt::ImhNone);
+        horizontalLayoutWidget = new QWidget(SecondWindow);
+        horizontalLayoutWidget->setObjectName(QStringLiteral("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(10, 10, 381, 151));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setSizeConstraint(QLayout::SetMinimumSize);
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(10);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setSizeConstraint(QLayout::SetMinimumSize);
+        verticalLayout->setContentsMargins(10, 10, 10, 10);
+        label_2 = new QLabel(horizontalLayoutWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+
+        verticalLayout->addWidget(label_2);
+
+        label_3 = new QLabel(horizontalLayoutWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+
+        verticalLayout->addWidget(label_3);
+
+        label_14 = new QLabel(horizontalLayoutWidget);
+        label_14->setObjectName(QStringLiteral("label_14"));
+
+        verticalLayout->addWidget(label_14);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        horizontalSpacer = new QSpacerItem(20, 30, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setSizeConstraint(QLayout::SetMinAndMaxSize);
+        verticalLayout_2->setContentsMargins(10, 10, 10, 10);
+        textEdit_4 = new QTextEdit(horizontalLayoutWidget);
+        textEdit_4->setObjectName(QStringLiteral("textEdit_4"));
+
+        verticalLayout_2->addWidget(textEdit_4);
+
+        textEdit_5 = new QTextEdit(horizontalLayoutWidget);
+        textEdit_5->setObjectName(QStringLiteral("textEdit_5"));
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(textEdit_5->sizePolicy().hasHeightForWidth());
+        textEdit_5->setSizePolicy(sizePolicy);
+
+        verticalLayout_2->addWidget(textEdit_5);
+
+        textEdit_15 = new QTextEdit(horizontalLayoutWidget);
+        textEdit_15->setObjectName(QStringLiteral("textEdit_15"));
+        sizePolicy.setHeightForWidth(textEdit_15->sizePolicy().hasHeightForWidth());
+        textEdit_15->setSizePolicy(sizePolicy);
+
+        verticalLayout_2->addWidget(textEdit_15);
+
+        verticalLayout_2->setStretch(0, 1);
+
+        horizontalLayout->addLayout(verticalLayout_2);
+
+
+        retranslateUi(SecondWindow);
+
+        QMetaObject::connectSlotsByName(SecondWindow);
+    } // setupUi
+
+    void retranslateUi(QDialog *SecondWindow)
+    {
+        SecondWindow->setWindowTitle(QApplication::translate("SecondWindow", "Dialog", 0));
+        label_2->setText(QApplication::translate("SecondWindow", "\320\235\320\260\320\270\320\274\320\265\320\275\320\276\320\262\320\260\320\275\320\270\320\265 \320\270\320\267\320\264\320\265\320\273\320\270\321\217", 0));
+        label_3->setText(QApplication::translate("SecondWindow", "\320\235\320\260\320\270\320\274\320\265\320\275\320\276\320\262\320\260\320\275\320\270\320\265 \320\270\320\267\320\264\320\265\320\273\320\270\321\217", 0));
+        label_14->setText(QApplication::translate("SecondWindow", "\320\227\320\260\320\262. \342\204\226 \320\270\320\267\320\264\320\265\320\273\320\270\321\217", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class SecondWindow: public Ui_SecondWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_SECONDWINDOW_H
